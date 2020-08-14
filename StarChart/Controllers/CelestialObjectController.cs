02666aa247baede613169f8e8bada9a6540f9d81
Prince Das <ppdctl@gmail.com>
2020-08-14 04:55:00 +0000

    Create CelestialObjectController's Get Actions

--- StarChart/Controllers/CelestialObjectController.cs
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -18,5 +18,49 @@ namespace StarChart.Controllers
         {
             this._context = context;
         }
+
+        [HttpGet("{id:int}",Name ="GetById")]
+        public IActionResult GetById(int id)
+        {
+            var celestialobject = this._context.CelestialObjects.Find(id);
+            if (celestialobject == null)
+            {
+                return NotFound();
+            }
+            var satellites = this._context.CelestialObjects.Where(s => s.OrbitedObjectId == id).ToList();
+            celestialobject.Satellites = satellites;
+            return Ok(celestialobject);
+        }
+
+        [HttpGet("{name}")]
+        public IActionResult GetByName(string name)
+        {
+            var celestialObjects = this._context.CelestialObjects.Where(c => c.Name == name).ToList();
+            if (celestialObjects == null || celestialObjects.Count == 0)
+            {
+                return NotFound();
+            }
+
+            foreach (var item in celestialObjects)
+            {
+                var satellites = this._context.CelestialObjects.Where(s => s.OrbitedObjectId == item.Id).ToList();
+                item.Satellites = satellites;
+            }
+
+
+            return Ok(celestialObjects);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var celestialObjects = this._context.CelestialObjects.ToList();
+            foreach (var item in celestialObjects)
+            {
+                var satellites = this._context.CelestialObjects.Where(s => s.OrbitedObjectId == item.Id).ToList();
+                item.Satellites = satellites;
+            }
+            return Ok(celestialObjects);
+        }
     }
 }
